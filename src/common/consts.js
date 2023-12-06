5b91485913b99b99e233dfd2acb301d6ded743e4
jlowery2663 <jlowery2663@gmail.com>
2023-12-06 13:49:08 -0800

    bump micro version; format consts.js

--- src/common/consts.js
+++ src/common/consts.js
@@ -1,14 +1,15 @@
-const APP_NAME = "Fenster"
-const VERSION = "1.2.1"
+const APP_NAME = "Fenster";
+const VERSION = "1.2.2";
 
 const INCR = 5; //list increment
 
-const COMPARE = 1
+const COMPARE = 1;
 
 const sortEnum = {
     EVALUATION: 1,
     NAME: 2,
     // RESULTS: 3,
+    // ECO: 4
 };
 
 const modes = {
@@ -17,18 +18,32 @@ const modes = {
     search: 3,
     pgnAnalyze: 4,
     about: 5,
+};
 
-}
+const SUBTITLES = [];
+SUBTITLES[modes.search] = "Search Openings";
+SUBTITLES[modes.admin] = "Test and Administration";
+SUBTITLES[modes.pgnAnalyze] = "Import and Analyze PGN";
+SUBTITLES[modes.about] = "About Fenster";
 
-const SUBTITLES = []
-SUBTITLES[modes.search] = "Search Openings"
-SUBTITLES[modes.admin] = "Test and Administration"
-SUBTITLES[modes.pgnAnalyze] = "Import and Analyze PGN"
-SUBTITLES[modes.about] = "About Fenster"
+const sites = ["FICS", "lichess", "shredder"];
+const siteUrls = {
+    FICS: "https://www.freechess.org/",
+    lichess: "https://lichess.org/",
+    shredder: "https://www.shredderchess.com/",
+};
 
-const sites = ["FICS", "lichess", "shredder"]
-const siteUrls = {FICS:"https://www.freechess.org/", lichess: "https://lichess.org/", shredder: "https://www.shredderchess.com/"}
+const FENEX = /(?!.*\d{2,}.*)^([1-8PNBRQK]+\/){7}[1-8PNBRQK]+$/im;
 
-const FENEX = /(?!.*\d{2,}.*)^([1-8PNBRQK]+\/){7}[1-8PNBRQK]+$/im
-
-export {APP_NAME, SUBTITLES, INCR, COMPARE, sortEnum, modes, sites, siteUrls, FENEX, VERSION}
+export {
+    APP_NAME,
+    SUBTITLES,
+    INCR,
+    COMPARE,
+    sortEnum,
+    modes,
+    sites,
+    siteUrls,
+    FENEX,
+    VERSION,
+};
