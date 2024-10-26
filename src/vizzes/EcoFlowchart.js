31f866b0943446f3130b660b414210ed4ed69908
jlowery2663 <jlowery2663@gmail.com>
2024-10-26 11:36:54 -0700

    fixed data errors in ecoCodes; added diags to viz of same

--- src/vizzes/EcoFlowchart.js
+++ src/vizzes/EcoFlowchart.js
@@ -2,34 +2,64 @@ import data from "../common/ecoCodes.js";
 import { ECO_CATS } from "../common/consts.js";
 import "./EcoFlowchart.css";
 import { useState } from "react";
-import {Chessboard} from "kokopu-react"
-import {Chess} from "chess.js"
+import { Chessboard } from "kokopu-react";
+import { Chess } from "chess.js";
 
-const chess = new Chess()
+const chess = new Chess();
 
 const EcoCats = ({ category }) => {
     const [cat, desc] = category;
-    const [active, setActive] = useState('')
-    const [contentStyle, setContentStyle] = useState('none')
-    
+    const [active, setActive] = useState("");
+    const [contentStyle, setContentStyle] = useState("none");
+
     const handler = (e) => {
-        setActive(active === 'active'? '' : 'active')
-        setContentStyle(contentStyle === 'block'? 'none' : 'block')
-    }
+        setActive(active === "active" ? "" : "active");
+        setContentStyle(contentStyle === "block" ? "none" : "block");
+    };
 
     return (
         <>
-            <button type="button" className={`collapsible font-cinzel " + ${active}`} style={{fontSize:'large'}} onClick={handler}>
+            <button
+                type="button"
+                className={`collapsible font-cinzel " + ${active}`}
+                style={{ fontSize: "large" }}
+                onClick={handler}
+            >
                 {cat}&mdash;{desc}
             </button>
-            <div className="content" style={{display:`${contentStyle}`, fontFamily:'Serif', fontSize:'larger'}}>
+            <div
+                className="content"
+                style={{
+                    display: `${contentStyle}`,
+                    fontFamily: "Serif",
+                    fontSize: "larger",
+                }}
+            >
                 {data[cat].map(([code, desc, moves, fen], i) => (
-                    <div>
-                    <span style={{borderBottom:'solid 1px', margin:'1em'}} key={fen + i}>
-                        <span style={{color:'lightgray', textShadow: '1px 1px 4px black'}}>{cat}
-                        {code}:{"   "}</span>{desc},{"  "}{moves}
-                    </span>
-                    <span display="inline"><Chessboard position={fen??"start"} squareSize={20}/></span>
+                    <div className="row" key={fen}>
+                        <span
+                            style={{ borderBottom: "solid 1px", margin: "1em" }}
+                            key={fen}
+                            className="column"
+                        >
+                            <span
+                                style={{
+                                    color: "lightgray",
+                                    textShadow: "1px 1px 4px black",
+                                }}
+                            >
+                                {cat}
+                                {code}:{"   "}
+                            </span>
+                            {desc},{"  "}
+                            {moves}
+                        </span>
+                        <span className="column right">
+                            <Chessboard
+                                position={fen ?? "start"}
+                                squareSize={20}
+                            />
+                        </span>
                     </div>
                 ))}
             </div>
@@ -38,9 +68,9 @@ const EcoCats = ({ category }) => {
 };
 
 const EcoFlowchart = () => (
-    <div style={{marginRight:'3em'}} >
+    <div style={{ marginRight: "3em" }}>
         {ECO_CATS.map((category) => (
-            <EcoCats {...{ category }} />
+            <EcoCats {...{ category }} key={category[1]} />
         ))}
     </div>
 );
