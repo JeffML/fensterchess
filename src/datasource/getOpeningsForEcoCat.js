4f6ecebd287d8946e8ac673eaaa4de1decc5accf
jlowery2663 <jlowery2663@gmail.com>
2025-07-07 10:41:28 -0700

    added dummy root for A00

--- src/datasource/getOpeningsForEcoCat.js
+++ src/datasource/getOpeningsForEcoCat.js
@@ -1,15 +1,26 @@
 import { getLatestEcoJson } from './getLatestEcoJson';
 
+//special case: A00 has no true root
+const A00Root = {
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1": {
+        "src": "none",
+        "eco": "A00",
+        "moves": "",
+        "name": "Irregular Openings",
+    }
+}
+
 export const getOpeningsForEcoCat = async (cat) => {
     const data = await getLatestEcoJson();
 
     const openings = data[cat].json;
-
-    const roots = Object.entries(openings)
+    
+    let roots = Object.entries(openings)
         .reduce((acc, [fen, opening]) => {
             if (opening.isEcoRoot) acc[fen] = opening
             return acc
         }, {})
-
+    
+    roots = {...A00Root, ...roots}
     return { openings, roots };
 };
