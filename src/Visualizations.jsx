39c75cbb26c260c92969618f16eae7d43d503d1d
jlowery2663 <jlowery2663@gmail.com>
2025-08-02 18:20:22 -0700

    code clean up

--- src/Visualizations.jsx
+++ src/Visualizations.jsx
@@ -14,83 +14,54 @@ const visualizations = [
     { name: PIECE_DESTINATION, type: "heatmap" },
 ];
 
-const Graphs = ({ graphs, handler }) => {
-    return (
-        <div id='graphs' className="grid-column">
-            {graphs.map((g) => (
-                <div
-                    key={g.name} className="grid-child"
-                >
-                    <label>
-                        <input
-                            type="radio"
-                            name="viz"
-                            value={g.name}
-                            onClick={handler}
-                            style={{ width: "1em" }}
-                        ></input>
-                        {g.name}
-                    </label>
-                </div>
-            ))}
-        </div>
-    );
-};
-
-const Heatmaps = ({ heatmaps, handler }) => {
-    return (
-        <div id="heatmaps" className="grid-column">
-            {heatmaps.map((g) => (
-                <div
-                    key={g.name} className="grid-child"
-                >
-                    <label>
-                        <input
-                            type="radio"
-                            name="viz"
-                            value={g.name}
-                            onClick={handler}
-                            style={{ width: "1em" }}
-                        ></input>
-                        {g.name}
-                    </label>
-                </div>
-            ))}
-        </div>
-    );
-};
+// Single component instead of Graphs + Heatmaps
+const VisualizationGroup = ({ items, handler, id }) => (
+    <div id={id} className="grid-column">
+        {items.map((item) => (
+            <div key={item.name} className="grid-child">
+                <label>
+                    <input
+                        type="radio"
+                        name="viz"
+                        value={item.name}
+                        onClick={handler}
+                        style={{ width: "1em" }}
+                    />
+                    {item.name}
+                </label>
+            </div>
+        ))}
+    </div>
+);
 
 const Visualizations = () => {
     const [viz, setViz] = useState("");
-    const handler = ({ target }) => {
-        setViz(target.value);
-    };
+    const handler = ({ target }) => setViz(target.value);
 
-    const graphs = visualizations.filter((o) => o.type === "graph");
-    const heatmaps = visualizations.filter((o) => o.type === "heatmap");
+    const graphs = visualizations.filter(o => o.type === "graph");
+    const heatmaps = visualizations.filter(o => o.type === "heatmap");
 
     return (
         <div className="white grid-style">
-            <div
-                style={{
-                    textAlign: "left",
-                    marginLeft: "1em",
-                }}
-            >
+            <div style={{ textAlign: "left", marginLeft: "1em" }}>
                 <h2 className="font-cinzel">Experimental Visualizations</h2>
+                
                 <div style={{ gridColumn: "1" }}>
                     <h3 className="left">Graphs & Lists</h3>
-                    <Graphs {...{ graphs, handler }}></Graphs>
+                    <VisualizationGroup items={graphs} handler={handler} id="graphs" />
                 </div>
+                
                 <div style={{ gridColumn: "1" }}>
                     <h3 className="left">Heatmaps</h3>
-                    <Heatmaps {...{ heatmaps, handler }}></Heatmaps>
+                    <VisualizationGroup items={heatmaps} handler={handler} id="heatmaps" />
                 </div>
             </div>
-            <Display {...{ viz }} />
+            
+            <Display viz={viz} />
         </div>
     );
 };
 
-export { ECO_FLOWCHART, FROM_TO, MOST_ACTIVE, PIECE_DESTINATION, Visualizations as default };
+export { ECO_FLOWCHART, FROM_TO, MOST_ACTIVE, PIECE_DESTINATION };
+export default Visualizations;
 
