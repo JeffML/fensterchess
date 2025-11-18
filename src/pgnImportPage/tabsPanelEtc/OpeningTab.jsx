3882ba108838329d517a9c8b2e090c9ca65c3731
Jeff Lowery <jlowery2663@gmail.com>
2025-11-18 09:39:48 -0800

    moved from yarn to npm; updated packages

--- src/pgnImportPage/tabsPanelEtc/OpeningTab.jsx
+++ src/pgnImportPage/tabsPanelEtc/OpeningTab.jsx
@@ -1,20 +1,19 @@
-import { Chess } from 'chess.js';
-import { useRef } from 'react';
-import { findOpeningForKokopuGame } from '../../utils/openings';
-import { OpeningDetails } from './openingTabContent/OpeningDetails';
-
+import { ChessPGN } from "@chess-pgn/chess-pgn";
+import { useRef } from "react";
+import { findOpeningForKokopuGame } from "../../utils/openings";
+import { OpeningDetails } from "./openingTabContent/OpeningDetails";
 
 export const OpeningTab = ({ game }) => {
-    const chess = useRef(new Chess());
+  const chess = useRef(new ChessPGN());
 
-    if (!game)
-        return (
-            <span className="white" style={{ fontSize: 'larger' }}>
-                Please select an opening from the Games tab
-            </span>
-        );
+  if (!game)
+    return (
+      <span className="white" style={{ fontSize: "larger" }}>
+        Please select an opening from the Games tab
+      </span>
+    );
 
-    const opening = findOpeningForKokopuGame(game);
-    chess.current.loadPgn(opening.moves);
-    return <OpeningDetails {...{ opening, game, chess }} />;
+  const opening = findOpeningForKokopuGame(game);
+  chess.current.loadPgn(opening.moves);
+  return <OpeningDetails {...{ opening, game, chess }} />;
 };
